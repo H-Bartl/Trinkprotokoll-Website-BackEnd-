54ed0f1c6f856b03fc826bbbfcc429842d111f22
Hamza Bartl <hamzabartl5@gmail.com>
2024-06-04 13:53:20 +0200

    JWT verifyPassword

--- src/services/JWTService.ts
+++ src/services/JWTService.ts
@@ -1,7 +1,36 @@
 import { LoginResource } from "../Resources";
+import { login } from "./AuthenticationService";
+import { JwtPayload, sign } from "jsonwebtoken";
+import { Pfleger } from "../model/PflegerModel";
 
 export async function verifyPasswordAndCreateJWT(name: string, password: string): Promise<string | undefined> {
-    throw new Error("Function verifyPasswordAndCreateJWT not implemented yet")
+    const einlogen = await login(name, password);
+    if(!einlogen){
+        throw new Error("Login fehlgeschlagen!")
+    }
+
+    let findPfleger = await Pfleger.findOne({name}).exec();
+    if(!findPfleger){
+        throw new Error("Kein regristierter Pfleger auf diesem Namen!")
+    }
+
+    const secret = process.env.JWT_SECRET;
+    const ttl = process.env.JWT_TTL;
+
+    const payload: JwtPayload = {
+        sub: findPfleger.id
+    }
+
+    const jwtString = sign(
+        payload,
+        secret!,
+        {
+            expiresIn: ttl,
+            algorithm: "HS256"
+        }
+    )
+
+    return jwtString;
 }
 
 export function verifyJWT(jwtString: string | undefined): LoginResource {
