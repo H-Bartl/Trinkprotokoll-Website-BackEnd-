479b466988e7d6e36cc91ef77051c1e96cc62d97
Hamza Bartl <hamzabartl5@gmail.com>
2024-06-05 19:28:25 +0200

    login router erstellt, erstmal ohne test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import "express-async-errors"; // needs to be imported before routers and other stuff!
 
-//import { loginRouter } from './routes/login';
+import { loginRouter } from './routes/login';
 // import { eintragRouter } from './routes/eintrag';
 // import { pflegerRouter } from './routes/pfleger';
 import { protokollRouter } from './routes/protokoll';
@@ -14,7 +14,7 @@ const app = express();
 app.use('*', express.json()) // vgl. Folie 138
 
 // Routes
-// app.use("/api/login", loginRouter)   // wird erst später implementiert, hier nur Dummy; hat aber bereits einen Präfix
+app.use("/api/login", loginRouter)   // wird erst später implementiert, hier nur Dummy; hat aber bereits einen Präfix
 app.use("/api/protokoll", protokollRouter)
 app.use("/api/pfleger", pflegerRouter)
 app.use("/api/eintrag", eintragRouter)
---
+++ src/routes/login.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import { body, matchedData, validationResult } from "express-validator";
+import { login } from "../services/AuthenticationService";
+import { verifyJWT, verifyPasswordAndCreateJWT } from "../services/JWTService";
+import { LoginResource } from "../Resources";
+
+
+export const loginRouter = express.Router();
+
+loginRouter.post("/",
+    body("name").isString().isLength({min: 1, max: 100}),
+    body("password").isStrongPassword().isString(),
+    async (req, res, next) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).send({errors: errors})
+        }
+        const ttl = parseInt(process.env.JWT_TTL!);
+        try {
+            const logreq = matchedData(req);
+            const jwtString = await verifyPasswordAndCreateJWT(logreq.name, logreq.password)
+            const verifyJwt = await verifyJWT(jwtString)
+
+            res.cookie("access_token", verifyJwt, {
+                httpOnly: true,
+                expires: new Date(Date.now() + ttl * 1000),
+                secure: true,
+                sameSite: "none"
+            })
+            res.status(201).send(verifyJwt);
+        } catch (err) {
+            res.status(401)
+            next(err)
+        }
+    })
+
+    loginRouter.get("/",async (req, res, next) => {
+        try {
+            const jwtString = req.cookies.access_token
+            const verifyJwt = verifyJWT(jwtString);
+            if(!verifyJwt){
+                res.clearCookie(jwtString)
+                return res.status(404).send(false)
+            }
+            res.status(200).send(verifyJwt);
+        } catch (err) {
+            res.status(404);
+            next(err)
+        }
+    })
+
+    loginRouter.delete("/",async (req, res, next) => {
+        const jwtString = req.cookies.access_token;
+        res.clearCookie(jwtString);
+        res.sendStatus(204)
+    })
